421ca4c46d3650161beaa6a9f944e87de04031b4
ScolzeWA <64407840+ScolzeWA@users.noreply.github.com>
2022-07-07 13:02:32 +0200

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("APP_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
+    DEV= os.environ.gey("DEV", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
